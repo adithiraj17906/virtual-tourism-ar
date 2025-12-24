3336be13bd0b7be967dec760fcfa5fc8c3524c88
justlikethat1709-ctrl <justlikethat1709@gmail.com>
2025-12-24 20:39:17 +0530

    Final product update and deployment prep

--- tourism_shop/settings.py
+++ tourism_shop/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
